0462bbef9e090e96c1f201300b412bcef96a2c06
subtanker <55813588+2subtanker@users.noreply.github.com>
2020-12-05 23:39:19 +0900

    Update todo.js 2

--- todo.js
+++ todo.js
@@ -4,7 +4,20 @@ toDoList = document.querySelector(".js-toDoList");
 
 
 const TODOS_LS = "toDos";
-const toDos = [];
+let toDos = [];
+
+function deleteToDo(event){
+    const btn = event.target;
+    const li = btn.parentNode;
+    toDoList.removeChild(li);
+
+    const cleanToDos = toDos.filter(function(toDo) {
+        return toDo.id !== parseInt(li.id);
+    });
+    toDos = cleanToDos;
+    saveToDos();
+    // filter는 array의 모든 아이템을 통해 함수를 실행
+}
 
 function paintToDo(text){
     const li = document.createElement("li");
@@ -12,6 +25,7 @@ function paintToDo(text){
     const span = document.createElement("span");
     const newId = toDos.length + 1;
     delBtn.innerText = "X";
+    delBtn.addEventListener("click", deleteToDo);
     span.innerText = text;
     li.appendChild(delBtn);
     li.appendChild(span);

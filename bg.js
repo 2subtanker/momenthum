881139d59a7e15a4e004463f973da624e8c82a11
2subtanker <2subtanker@gmail.com>
2021-01-23 16:05:30 +0900

    bg.js

--- bg.js
+++ bg.js
@@ -1,28 +1,26 @@
 const body = document.querySelector("body");
 
-const IMG_NUMBER =3;
+const IMG_NUMBER = 7;
 
-function handleImgLoad(){
-    console.log("finished loading image");
+function handleImgLoad() {
+  console.log("finished loading image");
 }
 
-function paintImage(imgNumber){
-    const image = new Image();
-    image.src = `./images/${imgNumber}.jpg`;
-    image.classList.add("bgImage");
-    body.appendChild(image);
-
+function paintImage(imgNumber) {
+  const image = new Image();
+  image.src = `./images/${imgNumber}.jpg`;
+  image.classList.add("bgImage");
+  body.appendChild(image);
 }
 
-function genRandom(){
-    const number = Math.floor(Math.random()*IMG_NUMBER+1);
-    return number;
+function genRandom() {
+  const number = Math.floor(Math.random() * IMG_NUMBER + 1);
+  return number;
 }
 
-function init(){
-
-    const randomNumber = genRandom();
-    paintImage(randomNumber);
+function init() {
+  const randomNumber = genRandom();
+  paintImage(randomNumber);
 }
 
 init();
